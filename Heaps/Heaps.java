d4cfaa5e33f8ef3df8f37ca1088025520975fbc6
toocolors <188366890+toocolors@users.noreply.github.com>
2026-02-12 23:12:00 +0000

    Created Heaps folder, added Heaps.java.

---
+++ Heaps/Heaps.java
@@ -0,0 +1,230 @@
+/*
+ * INSTRUCTION:
+ *     This is a Java staring code for hw5_1.
+ *     When you finish the development, download this file and and submit to Canvas
+ *     according to the submission instructions.
+
+ *     Please DO NOT change the name of the main class "Main"
+ */
+
+// Finish the head comment with Abstract, Name, and Date.
+/*
+ * Abstract: Takes a series of number and sorts them into a heap, then takes
+ *  extra operations to manipulate the heap.
+ * Name: Noah deFer
+ * Date: 2/10/2026
+ */
+import java.util.Scanner;
+import java.util.regex.Pattern;
+ 
+class Main 
+{
+
+    // Global variables
+    static int[] heap;
+    static int heapSize;
+    static String[] commands;
+
+    public static void main(String[] args) {
+        // Create Scanner Object
+        Scanner scanner = new Scanner(System.in);
+
+        // Get input size
+        int inputSize = scanner.nextInt();
+
+        // Initialize heap array
+        heap = new int[inputSize + 1];
+        heapSize = inputSize;
+
+        // Fill heap
+        for(int i = 1; i < heap.length; i++) {
+            // Get next value
+            heap[i] = scanner.nextInt();
+        }
+
+        // Get number of commands
+        int commandsSize = scanner.nextInt();
+
+        // Create commands array
+        commands = new String[commandsSize];
+
+        // Fill commands array
+        scanner.nextLine();
+        for(int i = 0; i < commands.length; i++) {
+            // Get next command
+            commands[i] = scanner.nextLine();
+        }
+
+        // Close Scanner Object
+        scanner.close();
+
+        // Print whether input is heap or not
+        if(isHeap()) {
+            System.out.println("This is a heap.");
+        } else {
+            System.out.println("This is NOT a heap.");
+        }
+
+        // Sort heap
+        sortHeap();
+
+        // Run commands
+        for(int i = 0; i < commands.length; i++) {
+            runCommand(commands[i]);
+        }
+    }
+    
+    /**
+     * Removes the first/largest element of the heap array and resorts it.
+     */
+    private static void deleteMax() {
+        // Swap max element and last element, delete max element
+        heap[1] = heap[heapSize];
+        heap[heapSize] = 0;
+        
+        // Update heapSize
+        heapSize--;
+
+        // Sort heap
+        sortHeap();
+    }
+    
+    /** 
+     * Prints the contents of the heap array.
+    */
+    private static void display() {
+        for(int i = 1; i <= heapSize; i++) {
+            System.out.print(heap[i] + " ");
+        }
+        System.out.println();
+    }
+
+    /**
+     * Prints each command in the commands array.
+     * Used for debugging.
+     */
+    private static void displayCommands() {
+        System.out.println("Printing commands...");
+        for(int i = 0; i < commands.length; i++) {
+            System.out.println("1: " + commands[i]);
+        }
+    }
+
+    /**
+     * Prints the largest/first element of the heap array.
+     */
+    private static void displayMax() {
+        System.out.println(heap[1]);
+    }
+
+    /**
+     * Inserts a number into the heap array then sorts it.
+     * Increases the size of the heap array if needed.
+     */
+    private static void insert(int num) {
+        // Update heapSize
+        heapSize++;
+
+        // Check if heap array is large enough
+        if(heapSize >= heap.length) {
+            // Create larger array
+            int[] newHeap = new int [heap.length * 2];
+            
+            // Copy heap into new array
+            for(int i = 1; i < heap.length; i++) {
+                newHeap[i] = heap[i];
+            }
+            
+            // Set heap to new array
+            heap = newHeap;
+        }
+
+        // Add num to end of heap array
+        heap[heapSize] = num;
+
+        // Sort heap array
+        sortHeap();
+    }
+
+    /**
+     * Returns whether the heap array is currently a heap.
+     */
+    private static boolean isHeap() {
+        for(int i = 1; i <= heapSize; i++) {
+            // Check if left child is smaller/larger than current node
+            if(i * 2 <= heapSize && heap[i * 2] > heap[1]) {
+                return false;
+            }
+
+            // Check if right child is smaller/larger than current node
+            if(i * 2 + 1 <= heapSize && heap[i * 2 + 1] > heap[1]) {
+                return false;
+            }
+            
+        }
+        return true;
+    }
+
+    /**
+     * 
+     */
+    private static void runCommand(String command) {
+        // Check if command is insert X
+        if(Pattern.matches("^insert [0-9]+$", command)) {
+            // Try to process number
+            try {
+                // Get number from command
+                int num = Integer.parseInt(command.substring(7));
+                
+                // Insert number
+                insert(num);
+                return;
+            } catch (NumberFormatException e) {}
+        }
+
+        // Check if command is deleteMax, display, displayMax, or other
+        switch(command) {
+            case "deleteMax":
+                deleteMax();
+                break;
+            case "display":
+                display();
+                break;
+            case "displayMax":
+                displayMax();
+                break;
+            default:
+                System.out.println(command + " is unknown.");
+                break;
+        }
+    }
+
+    /**
+     * Sorts the heap array into a heap.
+     */
+    private static void sortHeap() {
+        for(int i = heapSize / 2; i >= 1; i--) {
+            int k = i;
+            int v = heap[k];
+            boolean isHeap = false;
+
+            while(!isHeap && k * 2 <= heapSize) {
+                int j = k * 2;
+                if(j < heapSize) { // there are two children
+                    if(heap[j] < heap[j + 1]) {
+                        j++;
+                    }
+                }
+                if (v >= heap[j]) {
+                    isHeap = true;
+                } else {
+                    heap[k] = heap[j];
+                    k = j;
+                }
+            }
+
+            heap[k] = v;
+        }
+    }
+}
+
