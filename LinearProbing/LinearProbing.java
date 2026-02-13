aba1471de2435090ddb5d7801af8274e5fa087a6
toocolors <188366890+toocolors@users.noreply.github.com>
2026-02-13 23:08:58 +0000

    Added LinearProbing.java

---
+++ LinearProbing/LinearProbing.java
@@ -0,0 +1,235 @@
+/*
+ * Abstract: Allows the user to manipulate a Hash Table with Linear Probing.
+ * Name: Noah deFer
+ * Date: 
+ */
+import java.util.Scanner;
+import java.util.regex.Pattern;
+ 
+class Main 
+{
+
+    // Global variables
+    static String[] commands;
+    static double maxLoad = 0.5;
+    static int[] table;
+    static int tableSize;
+
+    public static void main(String[] args) {
+        // Create Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Get initial table size
+        tableSize = scanner.nextInt();
+
+        // Create hash table
+        table = initializeArray(tableSize);
+
+        // Get number of commands
+        int commandsSize = scanner.nextInt();
+
+        // Create commands array
+        commands = new String[commandsSize];
+
+        // Fill commands array
+        scanner.nextLine();
+        for(int i = 0; i < commandsSize; i++) {
+            commands[i] = scanner.nextLine();
+        }
+
+        // Close Scanner
+        scanner.close();
+
+        // Run commands
+        for(int i = 0; i < commands.length; i++) {
+            runCommand(commands[i]);
+        }
+    }
+
+    /**
+     * Calculates the load factor of the table.
+     */
+    private static double getLoad() {
+        // Calculate n
+        int n = 0;
+        for(int i = 0; i < table.length; i++) {
+            if(table[i] >= 0) {
+                n++;
+            }
+        }
+
+        // Calculate Load Factor
+        return n / (table.length + 0.0);
+    }
+
+    /**
+     * Prints the value of an index, or empty if there is none.
+     */
+    private static void displayStatus(int index) {
+        // Check if index is out of bounds
+        if(index < 0 || table.length <= index) {
+            System.out.println(index + " is out of bounds.");
+            return;
+        }
+
+        // Check status of index
+        if(table[index] >= 0) {
+            System.out.println(table[index]);
+        } else {
+            System.out.println("Empty");
+        }
+    }
+
+    /**
+     * Inserts a number into table,
+     *  then calculates the current load factor and resizes the table if needed.
+     */
+    private static void insert(int num) {
+        // Insert num
+        int i = num % table.length;
+        while(true) {
+            // Check if table[i] is empty
+            if(table[i] < 0) {
+                table[i] = num;
+                break;
+            }
+            
+            // Update i
+            if(i >= table.length - 1) {
+                i = 0;
+            } else {
+                i++;
+            }
+        }
+
+        // Calculate load factor and increase table size if needed
+        double load = getLoad();
+        if(load > maxLoad) {
+            resizeTable();
+        }
+    }
+
+    /**
+     * Creates an array, fills it with -1, and returns it.
+     */
+    private static int[] initializeArray(int size) {
+        // Create Array
+        int[] arr = new int[size];
+
+        // Fill array with -1
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = -1;
+        }
+
+        // Return array
+        return arr;
+    }
+
+    /**
+     * Finds the next prime number after n
+     */
+    private static int nextPrime(int n) {
+        n *= 2;
+        boolean prime = false;
+        while(!prime) {
+            // Increment n
+            n++;
+
+            // Reset prime boolean
+            prime = true;
+
+            // Check if n has factors
+            for(int i = 2; i < n; i++) {
+                if(n % i == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+        }
+        
+        return n;
+    }
+
+    /**
+     * Increases the table size and rehashes all values.
+     */
+    private static void resizeTable() {
+        // Create copy of table
+        int[] copy = table.clone();
+
+        // Get new table size
+        int newSize = nextPrime(table.length);
+
+        // Reinitialize Table
+        table = initializeArray(newSize);
+
+        // Rehash table
+        for(int i = 0; i < copy.length; i++) {
+            if(copy[i] >= 0) {
+                insert(copy[i]);
+            }
+        }
+    }
+
+    private static void runCommand(String command) {
+        // Check if command is insert X
+        if(Pattern.matches("^insert [0-9]+$", command)) {
+            insert(Integer.parseInt(command.substring(7)));
+            return;
+        }
+
+        // Check if command is search X
+        if(Pattern.matches("^search [0-9]+$", command)) {
+            search(Integer.parseInt(command.substring(7)));
+            return;
+        }
+
+        // Check if command is displayStatus X
+        if(Pattern.matches("^displayStatus [0-9]+$", command)) {
+            displayStatus(Integer.parseInt(command.substring(14)));
+            return;
+        }
+
+        // Check if command is tableSize or unknown
+        switch(command) {
+            case "tableSize":
+                System.out.println(table.length);
+                break;
+            default:
+                System.out.println(command + " is unknwon");
+                break;
+        }
+    }
+
+    /**
+     * Searches for num within the table, and prints the result.
+     */
+    private static void search(int num) {
+        // Get hash of num
+        int i = num % table.length;
+        
+        boolean found = false;
+        do {
+            // Check if table[i] is num
+            if(table[i] == num) {
+                found = true;
+                break;
+            }
+            
+            // Update i
+            if(i >= table.length - 1) {
+                i = 0;
+            } else {
+                i++;
+            }
+        } while(i != num % table.length);
+
+        // Print result
+        if(found) {
+            System.out.println(num + " Found");
+        } else {
+            System.out.println(num + " Not Found");
+        }
+    }
+}
+
